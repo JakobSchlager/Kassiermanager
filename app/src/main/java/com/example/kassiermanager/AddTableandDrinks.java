733ca82d71835fdb51673a034863678646f0665e
Thomss37 <56640736+Thomss37@users.noreply.github.com>
2020-06-09 20:38:12 +0200

    Stammtische können lokal erstellt werden (ohne cloud) QR code wird erstellt von ID QR code kann gelesen werden, durch kamera.

---
+++ app/src/main/java/com/example/kassiermanager/AddTableandDrinks.java
@@ -0,0 +1,14 @@
+package com.example.kassiermanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddTableandDrinks extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_table_and_drinks);
+    }
+}
